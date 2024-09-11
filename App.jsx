0401b47d7c5e7951255836dcc9e65110d4199233
SarthakPanse <pansesarthak2707@gmail.com>
2024-09-11 15:53:23 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import navbar from './components/navbar/navbar'
+const App = () => {
+  return (
+    <div className='app'>
+      
+      <navbar/>
+
+    </div>
+  )
+}
+
+export default App
